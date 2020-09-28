d175ddb1eedcb99630b1c5b8a6fb80bb4358a131
De Mot Daan <daan.demot44@gmaim.com>
2020-09-28 17:27:29 +0200

    Toevoeging van de Welcome Class Toevoeging van de 3 get en set van dag, maand, jaar

---
+++ Welcome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeCalculator
+{
+    class Welcome
+    {
+        private int dag;
+        public int Dag
+        {
+            get {return dag;}
+            set { dag = value; }
+        }
+
+        private int maand;
+        public int Maand
+        {
+            get { return maand; }
+            set { maand = value; }
+        }
+        private int jaar;
+        public int Jaar
+        {
+            get {return jaar;}
+            set { jaar = value; }
+        }
+
+
+
+
+    }
+}
