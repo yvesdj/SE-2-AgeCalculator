9598d633785240ce7d7fd9a337d2b3ddd0501a30
yvesdj <yves.dejonghe@student.ap.be>
2020-10-06 17:11:41 +0200

    Application can now be restarted or closed with StateManager

--- Output.cs
+++ Output.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AgeCalculator
@@ -24,7 +25,26 @@ namespace AgeCalculator
 
         public void CloseOrRestart()
         {
+            Console.WriteLine("\nDo you want to calculate again?");
+            Console.WriteLine("Type 'yes' to restart or 'no' to close the application.");
+            string answ = Console.ReadLine();
 
+            switch (answ)
+            {
+                case "yes":
+                    Console.WriteLine("The application will restart momentarily.");
+                    Thread.Sleep(1000);
+                    StateManager.Instance.Run();
+                    break;
+
+                case "no":
+                    Console.WriteLine("The application will close momentarily");
+                    Thread.Sleep(1000);
+                    StateManager.Instance.Exit();
+                    break;
+                default:
+                    break;
+            }
         }
 
         public void PrintOutput()
@@ -34,6 +54,8 @@ namespace AgeCalculator
             Console.WriteLine("Age: " + ageCalculator.age);
 
             Console.WriteLine("\nYou will die on the following date: " + deathCalculator.RandomDate.ToString("dddd, dd MMMM yyyy"));
+
+            CloseOrRestart();
         }
     }
 }
